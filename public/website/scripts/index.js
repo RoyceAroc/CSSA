40a7e164b8b228904ceead650563f6d7bf0961ba
Dr AlienTech <alientechgamig@hotmail.com>
2021-01-14 13:36:36 -0500

    added home page animation

---
+++ public/website/scripts/index.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+    let i = 0;
+
+    setInterval(function () {
+        if (i < 20) {
+            document.getElementById("header").style.paddingTop = `${17 - 0.7 * (i + 1)}%`;
+            // document.getElementById("subtitle").style.marginTop = `${i + 1}%`;
+            document.getElementById("header-logo").style.width = `${1 * (i + 1)}%`;
+
+            i++;
+        }
+    }, 50);
+}
