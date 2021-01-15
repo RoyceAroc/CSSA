f867a0faf3cac190a3ceae1298e81d12bb67a383
Royce Arockiasamy <royceaden@gmail.com>
2021-01-15 17:54:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require("path");
 var url = require('url');
 const express = require("express");
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const { parse } = require('querystring');
 const { Connection, Request } = require("tedious");
 
