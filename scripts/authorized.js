c7e690ecbd6f706160b55cd3f05cb76350d641d4
Royce Arockiasamy <royceaden@gmail.com>
2021-01-17 11:48:18 -0500

    Update authorized.js

--- scripts/authorized.js
+++ scripts/authorized.js
@@ -136,6 +136,7 @@ function generatePassword() {
 function signOut() {
 	var auth2 = gapi.auth2.getAuthInstance();
 	auth2.signOut().then(function () {
+		console.log("User has signed out!");
 		// User has successfully signed out
 	});
 }
