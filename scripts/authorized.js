bad9b385cdd31fdb6095a3dcff8ee3646b53117d
Royce Arockiasamy <royceaden@gmail.com>
2021-01-17 12:19:02 -0500

    Update authorized.js

--- scripts/authorized.js
+++ scripts/authorized.js
@@ -134,10 +134,11 @@ function generatePassword() {
 }
 
 function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
+	var auth2 = gapi.auth2.getAuthInstance();
+auth2.signOut().then(function () {
+	console.log("k");
+});
+auth2.disconnect();
 }
 
 function setCookie(name,value,days) {
