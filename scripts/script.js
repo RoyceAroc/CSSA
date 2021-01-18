709d5408d6211ab7329ce4b71677ecc236fdeaaa
Royce Arockiasamy <royceaden@gmail.com>
2021-01-18 10:00:13 -0500

    automatic cookie check

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,27 @@
 window.addEventListener('load', function () {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementsByTagName("nav")[0].innerHTML = xhttp.responseText;
-        } else if (this.readyState == 4 && this.status != 200) {
-            xhttp.send();
-        }
-    };
+    if(getCookie('email') != null) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementsByTagName("nav")[0].innerHTML = xhttp.responseText;
+            } else if (this.readyState == 4 && this.status != 200) {
+                xhttp.send();
+            }
+        };
 
-    xhttp.open("GET", "navbar.html", true);
-    xhttp.send();
+        xhttp.open("GET", "navbarA.html", true);
+        xhttp.send();
+    } else {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementsByTagName("nav")[0].innerHTML = xhttp.responseText;
+            } else if (this.readyState == 4 && this.status != 200) {
+                xhttp.send();
+            }
+        };
+
+        xhttp.open("GET", "navbarB.html", true);
+        xhttp.send();
+    }
 });
