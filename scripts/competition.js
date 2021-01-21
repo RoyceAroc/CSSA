90cdbc40c8e37bac0f5ab0891fc214421d6b3982
Dr AlienTech <alientechgamig@hotmail.com>
2021-01-21 00:05:55 -0500

    bodge calendar that works but must be updated in the future

--- scripts/competition.js
+++ scripts/competition.js
@@ -37,7 +37,7 @@ function populateCompetitionPage(eventData) {
     xmlDoc = parser.parseFromString(eventData, "text/xml");
 
     for (events of xmlDoc.getElementsByTagName("events")) {
-        for (i in events.childNodes) {
+        for (let i in events.childNodes) {
             if (i % 2 == 1) {
                 let e = events.childNodes[i];
     
@@ -56,4 +56,25 @@ function populateCompetitionPage(eventData) {
             }
         }
     }
+
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let d = new Date();
+    let year = d.getFullYear();
+    let month = months[d.getMonth()];
+    let day = days[d.getDay()];
+
+    let seed = 7 + d.getDay();
+
+    let startDate = d.getDate() - d.getDay() - 8;
+
+    for (let i = 0; i < 35; i++) {
+        document.getElementById(i).innerHTML = (startDate + i) % 31 + 1;
+    }
+
+};
+
+function daysInMonth (month, year) {
+    return new Date(year, month, 0).getDate();
 }
