cf000c4230cec0942b4e6230756209488e453616
Dr AlienTech <alientechgamig@hotmail.com>
2021-01-31 13:45:29 -0500

    nav fix

--- scripts/script.js
+++ scripts/script.js
@@ -15,7 +15,7 @@ window.addEventListener('load', function () {
             var xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
-                    document.getElementsByTagName("nav")[1].innerHTML = xhttp.responseText;
+                    document.getElementsByTagName("nav")[0].innerHTML = xhttp.responseText;
                 } else if (this.readyState == 4 && this.status != 200) {
                     xhttp.send();
                 }
