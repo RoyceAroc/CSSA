008cee235a71c691a835c1c18f00db0258b331b9
Royce Arockiasamy <royceaden@gmail.com>
2021-01-31 09:53:26 -0500

    updated seo optimization

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,12 @@
+fetch('scripts/seo_optimization.json')
+.then(response => response.text())
+.then(structuredDataText => {
+  const script = document.createElement('script');
+  script.setAttribute('type', 'application/ld+json');
+  script.textContent = structuredDataText;
+  document.head.appendChild(script);
+})
+
 window.addEventListener('load', function () {
     if (getCookie('email') != null) {
         if (window.location.href.includes("sign.html")) {
