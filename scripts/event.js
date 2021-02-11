ce14e21c1b076038a9a221ea5c070883b0fbebaa
Dr AlienTech <alientechgamig@hotmail.com>
2021-02-11 00:01:29 -0500

    updates

--- scripts/event.js
+++ scripts/event.js
@@ -3,54 +3,44 @@ var eventName;
 window.addEventListener('load', function () {
     var urlParams = new URLSearchParams(decodeURIComponent(window.location.search));
     eventName = urlParams.get('event');
-    console.log(eventName)
 
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            populateEventPage(xhttp.responseText);
-        }
-    };
-
-    xhttp.open("GET", "https://www.cssa.dev/xml/eventData.xml", true);
-    xhttp.send();
+    populateEventPage();
 });
 
-function populateEventPage(eventData) {
+function populateEventPage() {
     let eventTitle = document.getElementById("title");
     let eventDescription = document.getElementById("description");
 
     let infoName = document.getElementById("info-name");
     let infoImage = document.getElementById("info-image");
-    // Other fields...
 
-    var parser = new DOMParser();
-    xmlDoc = parser.parseFromString(eventData, "text/xml");
-    
-    var doc = $.parseXML(document.body);
-    $xml = $(doc);
-    $title = $xml.find("events");
-    console.log($title.text());
+    const event = eventData["events"][eventName];
 
-    var nodes = xml.evaluate("events/event", eventData, null, XPathResult.ANY_TYPE, null);
-    var result = nodes.iterateNext();
-    while (result) {
-        txt += result.childNodes[0].nodeValue + "<br>";
-        result = nodes.iterateNext();
-    } 
+    eventTitle.innerHTML = eventName;
+    eventDescription.innerHTML = event["description"];
 
-    for (events of xmlDoc.getElementsByTagName("events")) {
-        for (let i in events.childNodes) {
-            if (i % 2 == 1 && events.childNodes[i].childNodes[1].innerHTML == eventName) {
-                var e = events.childNodes[i];
-                console.log(e.childNodes);
+    infoName.innerHTML = eventName;
+    infoImage.src = event["image"];
 
-                eventTitle.innerHTML = eventName;
-                eventDescription.innerHTML = e.childNodes[9].innerHTML;
+    var createdTypes = [];
 
-                infoName.innerHTML = eventName;
-                infoImage.src = e.childNodes[3].innerHTML;
-            }
+    for (const resource of event["resources"]) {
+        let resourceHTML = `
+            <div class="resource" onclick="window.open('${resource["link"]}')">
+                &rarr;            <a>${resource["title"]}</a>
+            </div>
+        `;
+
+        if (!createdTypes.includes(resource["type"])) {
+            let resourceTypeHTML = `
+                <div id="${resource["type"]}" class="resourceType">
+                    <h5>${resource["type"]}</h5>
+                </div>
+            `;
+
+            document.getElementById("resources").innerHTML += resourceTypeHTML;
         }
+
+        document.getElementById(resource["type"]).innerHTML += resourceHTML;
     }
 }
