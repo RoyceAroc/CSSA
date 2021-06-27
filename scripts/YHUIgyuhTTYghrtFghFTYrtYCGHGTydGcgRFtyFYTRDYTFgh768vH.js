83425b5cee4f1a4bb476d14522b08c1b4b809e0b
Royce Arockiasamy <royceaden@gmail.com>
2021-06-27 10:26:10 -0400

    updates

--- scripts/YHUIgyuhTTYghrtFghFTYrtYCGHGTydGcgRFtyFYTRDYTFgh768vH.js
+++ scripts/YHUIgyuhTTYghrtFghFTYrtYCGHGTydGcgRFtyFYTRDYTFgh768vH.js
@@ -2,47 +2,6 @@ var globalSynchronizedPassword = "";
 
 var events = ""; var completed = true;
 
-function setCookie(name, value, days) {
-	var expires = "";
-
-	if (days) {
-		var date = new Date();
-		date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-		expires = "; expires=" + date.toUTCString();
-	}
-
-	document.cookie = name + "=" + (value || "") + expires + "; path=/";
-}
-
-function getCookie(name) {
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-
-	for (var i = 0; i < ca.length; i++) {
-		var c = ca[i];
-		while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-	}
-
-	return null;
-}
-
-function eraseCookie(name) {
-	document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-}
-
-function generatePassword() {
-	var length = 8,
-		charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-		retVal = "";
-
-	for (var i = 0, n = charset.length; i < length; ++i) {
-		retVal += charset.charAt(Math.floor(Math.random() * n));
-	}
-
-	return retVal;
-}
-
 function displayDashboard() {
 	document.getElementById("preloader").style.display = "none";
 	document.getElementById("competitions").style.display = "block";
--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,47 @@ function setLabel(label, text) {
     $('#setLabel').modal('show');
 }
 
+function setCookie(name, value, days) {
+	var expires = "";
+
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+		expires = "; expires=" + date.toUTCString();
+	}
+
+	document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+	}
+
+	return null;
+}
+
+function eraseCookie(name) {
+	document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+function generatePassword() {
+	var length = 8,
+		charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+		retVal = "";
+
+	for (var i = 0, n = charset.length; i < length; ++i) {
+		retVal += charset.charAt(Math.floor(Math.random() * n));
+	}
+
+	return retVal;
+}
+
 firebase.initializeApp({
     apiKey: "AIzaSyAPTvz8weUBIMyjl6ekC1uegX-j4u2Z1sc",
     authDomain: "cssa-dev.firebaseapp.com",
